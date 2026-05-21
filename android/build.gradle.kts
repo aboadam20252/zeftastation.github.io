cd0bb3183f2ab06f4f9fbbb646defd575189b0ee
adam-20266 <ahmedeldaby2025@gmail.com>
2026-05-21 17:30:00 +0300

    fix: add JAXB dependencies for Java 17 compat

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,10 @@
+buildscript {
+    dependencies {
+        classpath("javax.xml.bind:jaxb-api:2.3.1")
+        classpath("org.glassfish.jaxb:jaxb-runtime:2.3.8")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.2" apply false
 }
